62dd2fb97c23c7bcac3d4cdb2566816838e1e8c6
shubham <shubhammishra1442@gmail.com>
2024-12-13 23:56:58 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='us_visa_approval_prediction',
+    version='0.0.0',
+    author='shubham',
+    author_email='shubhammishra1442@gmail.com',
+    packages=find_packages()
+)
