b71d3d460bdab73245f31ccfb18f80ad5e80bdd2
shubham <shubhammishra1442@gmail.com>
2024-12-14 20:57:33 +0530

    exception handling

--- demo.py
+++ demo.py
@@ -1,4 +1,9 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
 
+import sys
+try:
+    a = 1/'10'
 
-logging.info(' logging system ')
+except Exception as e:
+    raise USvisaException(e,sys) from e
